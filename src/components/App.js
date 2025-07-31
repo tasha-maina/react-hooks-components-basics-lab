1abdb7b07bcba869186168604ac808841aedae2b
tasha maina <tashabanks271@gmail.com>
2025-07-31 20:49:54 +0300

    Completed react-hooks-lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import About from "./About";
 
 function NavBar() {
   return (
@@ -16,15 +17,12 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
-
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
